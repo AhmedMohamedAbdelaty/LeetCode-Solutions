fd1291ee6a04e9e62c091ab797a1e1da045217fd
Ahmed Mohamed <73834838+AhmedMohamedAbdelaty@users.noreply.github.com>
2023-12-13 18:17:45 +0200

    Time: 64 ms (99.56%), Space: 93.9 MB (8.02%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices)
     {
-        int mx = -1, ans = -1;
-        for (int i = prices.size() - 1; i >= 0; i--) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int mx = -1, ans = -1, n = prices.size();
+        for (int i = n - 1; i >= 0; i--) {
             mx = max(mx, prices[i]);
             ans = max(ans, (mx - prices[i]));
         }
