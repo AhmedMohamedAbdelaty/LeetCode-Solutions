d53ff6f71956a0b3cb8274e38adc47a2f746c11b
Ahmed Mohamed <73834838+AhmedMohamedAbdelaty@users.noreply.github.com>
2023-12-15 19:56:13 +0200

    Time: 77 ms (98.23%), Space: 69.8 MB (73.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums)
     {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
         unordered_map<int, int> mp;
         for (auto& x : nums) {
             mp[x]++;
