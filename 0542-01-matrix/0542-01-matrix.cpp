66f49539945455384bff8d61de0cee6f9ef8cebe
Ahmed Mohamed <73834838+AhmedMohamedAbdelaty@users.noreply.github.com>
2023-12-16 15:42:48 +0200

    Time: 60 ms (78.21%), Space: 30.8 MB (72.75%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,42 +1,34 @@
 class Solution {
     int n, m;
-    vector<vector<bool>> vis;
+
     int dx[4] = { 0, 0, -1, 1 };
     int dy[4] = { -1, 1, 0, 0 };
 
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat)
     {
-        n = mat.size();
-        m = mat[0].size();
-        vis.resize(n, vector<bool>(m, false));
-        vector<vector<int>> ans(n, vector<int>(m, 0));
+        n = mat.size(), m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, -1));
         queue<pair<int, int>> q;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
                     q.push({ i, j });
-                    vis[i][j] = true;
+                    ans[i][j] = 0;
                 }
             }
         }
-        int dist = 0;
         while (!q.empty()) {
-            int sz = q.size();
-            while (sz--) {
-                auto p = q.front(); // {x, y}
-                q.pop();
-                int x = p.first, y = p.second;
-                ans[x][y] = dist;
-                for (int i = 0; i < 4; ++i) {
-                    int nx = x + dx[i], ny = y + dy[i];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]) {
-                        vis[nx][ny] = true;
-                        q.push({ nx, ny });
-                    }
+            auto p = q.front();
+            q.pop();
+            int x = p.first, y = p.second;
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] == -1) {
+                    ans[nx][ny] = ans[x][y] + 1;
+                    q.push({ nx, ny });
                 }
             }
-            dist++;
         }
         return ans;
     }
