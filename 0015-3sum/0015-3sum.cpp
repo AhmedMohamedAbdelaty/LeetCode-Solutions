69c9a062ece003f4d5884f99c04af8289a07f1a9
Ahmed Mohamed <73834838+AhmedMohamedAbdelaty@users.noreply.github.com>
2023-12-17 17:20:37 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int low = i + 1, high = n - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+
+                if (sum == 0) {
+                    ans.push_back({ nums[i], nums[low], nums[high] });
+
+                    // duplicate removal
+                    while (low < high && nums[low] == nums[low + 1])
+                        low++;
+                    while (low < high && nums[high] == nums[high - 1])
+                        high--;
+
+                    low++, high--;
+                } else if (sum < 0)
+                    low++;
+                else
+                    high--;
+            }
+        }
+        return ans;
+    }
+};
