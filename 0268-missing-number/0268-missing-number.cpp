0b98dbb4f8b966c02cbf68a606a7c94e729a49bd
Ahmed Mohamed <73834838+AhmedMohamedAbdelaty@users.noreply.github.com>
2024-01-06 00:29:54 +0200

    Time: 19 ms (33.87%), Space: 18.4 MB (15.78%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        long long sum = (n*(n+1))/2, sumArr=0;
+        for (int i = 0;i<n;i++){
+            sumArr+=nums[i];
+        }
+        int diff= sum - sumArr;
+        return diff;
+    }
+};
